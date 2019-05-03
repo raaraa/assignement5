50a02437aac38814c22a20708838c9390d1b54b9
Raul <tonyrenteria14@gmail.com>
2019-05-03 10:26:23 -0500

    inital commit

---
+++ app/src/main/java/com/example/newsgateway/Article.java
@@ -0,0 +1,47 @@
+package com.example.newsgateway;
+
+import java.io.Serializable;
+
+public class Article implements Serializable {
+
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+
+    public Article(String author, String title, String description, String url, String urlToImage, String publishedAt) {
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+}
