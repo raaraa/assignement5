837a5e757ab43c18f49242ebb60e9888c78004b0
Raul <tonyrenteria14@gmail.com>
2019-05-04 10:45:39 -0500

    added menu items

--- app/src/main/java/com/example/newsgateway/MainActivity.java
+++ app/src/main/java/com/example/newsgateway/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.newsgateway;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
